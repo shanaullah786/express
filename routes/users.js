dee134f3f4b50fd01a468c4d673ea693647cd369
Shanaullah Khan <shanaullah786@gmail.com>
2020-02-17 08:37:38 +0530

    user name updated

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shanaullah Khan!' });
+  res.render('index', { title: 'Hi, Shan!' });
 });
 
 module.exports = router;
